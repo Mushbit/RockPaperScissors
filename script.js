41f6a4788dfe3968c5c31c01793124ac5f394e6c
Max <mushbit@protonmail.com>
2022-08-11 11:49:13 +0200

    buttons, score counter, display one message per round

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
-// Computer picks randomly
+const score = document.querySelector('div')
+const text = document.querySelector('#text');
+let compPoint = 0;
+let playerPoint = 0;
+score.textContent = `Computer: 0 Player: 0`;
+let buttons = document.querySelectorAll('button');
+
+function updateScore(compPoint, playerPoint) {
+    score.textContent=`Computer: ${compPoint} Player: ${playerPoint}`;
+}
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', (e) => {
+        console.log(e.target.id)
+        let computerSelection = computerPlay()
+        let result = playRound(computerSelection, e.target.id)
+
+        let para = document.createElement('p');
+        para.textContent = result;
+        text.appendChild(para);
+        score.textContent = `Computer: ${compPoint}
+        Player: ${playerPoint}`;
+
+        if (compPoint === 5) {
+            alert('Computer wins!');
+            compPoint = 0;
+            playerPoint = 0;
+            updateScore(0, 0)
+            let paras = document.querySelectorAll('p');
+            paras.forEach((Element) => {
+                text.removeChild(Element);
+            })
+        } else if (playerPoint === 5) {
+            alert('Computer wins!');
+            compPoint = 0;
+            playerPoint = 0;
+            updateScore(0, 0)
+            let paras = document.querySelectorAll('p');
+            paras.forEach((Element) => {
+                text.removeChild(Element);
+            })
+        }
+    })
+})
+
+
+
+
+
+
 function computerPlay() {
 
     switch ( Math.floor( Math.random() * 3 ) + 1 ) {
@@ -15,59 +65,63 @@ function computerPlay() {
 
 
 
-// Play a single round
 function playRound(computerSelection, playerSelection) {
 
-    // If computerPlay() picks rock
     if ( computerSelection === 'rock' ) {
         
-        switch (playerSelection.toLowerCase()) {
-            case 'rock':
+        switch (playerSelection) {
+            case 'rock': {
                 return 'Conk! Two rock\'s! Tied!';
-            case 'paper':
+            };
+            case 'paper': {
+                playerPoint++;
                 return 'Wrap you, rock! Player wins!';
-            case 'scissors':
+            };
+            case 'scissors': {
+                compPoint++;
                 return 'You rocked up my scissors! Computer wins!';
-            default:
-                return 'Gibberish! Computer Wins by default!';
+            };
+            default: {
+                return 'Gibberish! Computer Wins by default!'
+            };
         }
-    // If computerPlay() picks paper
+
     } else if (computerSelection === 'paper') {
 
-        switch (playerSelection.toLowerCase()) {
-            case 'rock':
-                return 'Your paper wrapped my rock up good! Computer wins!';
-            case 'paper':
-                return 'Two paper won\'t do much. Tied!';
-            case 'scissors':
-                return 'Snip Snap, cut you in a thousand pieces! Player wins!';
-            default:
-                return 'Gibberish! Computer Wins by default!';
+        switch (playerSelection) {
+            case 'rock': {
+                compPoint++
+                return 'Your paper wrapped my rock up good! Computer wins!'
+            };
+            case 'paper': {
+                return 'Two paper won\'t do much. Tied!'
+            };
+            case 'scissors': {
+                playerPoint++;
+                return 'Snip Snap, cut you in a thousand pieces! Player wins!'
+            };
+            default: {
+                return 'Gibberish! Computer Wins by default!'
+            };
         }
-    // Else computerPlay() picks scissors
+
     } else {
 
-        switch (playerSelection.toLowerCase()) {
-            case 'rock':
-                return 'I will, I will, rock you...r scissors! Player wins!';
-            case 'paper':
-                return 'your scissors cut me to pieces! Computer wins!';
-            case 'scissors':
-                return 'When fighting with scissors, no one wins! Tied!';
-            default:
-                return 'Gibberish! Computer Wins by default!';
+        switch (playerSelection) {
+            case 'rock': {
+                playerPoint++;
+                return 'I will, I will, rock you...r scissors! Player wins!'
+            };
+            case 'paper': {
+                compPoint++
+                return 'your scissors cut me to pieces! Computer wins!'
+            };
+            case 'scissors': {
+                return 'When fighting with scissors, no one wins! Tied!'
+            };
+            default: {
+                return 'Gibberish! Computer Wins by default!'
+            };
         }
     }
-}
-// Play 5 rounds
-function game() {
-    let result;
-    for (let i = 0; i < 5; i++) {
-        // Player prompted to picks rock, paper or scissors
-        let playerSelection = prompt( 'What will your hand be?')
-        // Calls computerPlay() to pick randomly
-        let computerSelection = computerPlay()
-        result = playRound(computerSelection, playerSelection);
-        console.log(result);
-    }  
 }
